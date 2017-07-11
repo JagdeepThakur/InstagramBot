e26e1d2b8dd16f8cbe8713addf5f76c917a27f91
JagdeepThakur <thakurjagdeep55@gmail.com>
2017-07-11 19:27:19 +0530

    File containing Url and access token created...!!

---
+++ token.py
@@ -0,0 +1,10 @@
+# This python file contains access token that is same for all other files
+
+# Access token to access Instagram API
+
+
+APP_ACCESS_TOKEN="5683883504.f45670f.213bbc20aee341eb8401b5be6678b236"
+
+# This Url is same for all other files
+
+BASE_URL="https://api.instagram.com/v1/"
